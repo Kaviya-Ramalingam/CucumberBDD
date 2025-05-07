7c22734b6da6f5827ac98295a95283b9718edc7c
Kaviya-Ramalingam <rkbevit@gmail.com>
2025-05-07 10:54:24 -0400

    DSALGO-369 excel reader class

--- src/test/java/dsalgo/utilities/ExcelReader.java
+++ src/test/java/dsalgo/utilities/ExcelReader.java
@@ -6,45 +6,89 @@ import java.util.ArrayList;
 import java.util.Iterator;
 
 import org.apache.poi.ss.usermodel.Cell;
+import org.apache.poi.ss.usermodel.CellType;
 import org.apache.poi.ss.usermodel.Row;
+import org.apache.poi.ss.util.NumberToTextConverter;
 import org.apache.poi.xssf.usermodel.XSSFSheet;
 import org.apache.poi.xssf.usermodel.XSSFWorkbook;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 
 public class ExcelReader {
-	FileInputStream fis;
-	public String path;
-	XSSFWorkbook workbook;
+
+	private static final Logger logger = LoggerFactory.getLogger(ExcelReader.class);
+	FileInputStream fis;// File input stream to read the Excel file
+	public String path;// file path to excel sheet
+	XSSFWorkbook workbook; // Workbook and Sheet objects from Apache POI
 	XSSFSheet sheet;
-	
+
+	// Constructor that receives the Excel file path
 	public ExcelReader(String path) {
-		
 		this.path = path;
 	}
 
-	public ArrayList<String> getData(String sheetName) throws IOException{
+	public ArrayList<String> getData(String sheetName, String testCaseIdName) throws IOException {
+		// Open the Excel file
 		fis = new FileInputStream(path);
-		ArrayList<String>a = new ArrayList<String>();
 		workbook = new XSSFWorkbook(fis);
 		int sheets = workbook.getNumberOfSheets();
-		
-		for(int i=0;i<=sheets;i++) {
-			if(workbook.getSheetName(i).equalsIgnoreCase(sheetName)){
-				sheet=workbook.getSheetAt(i);
-			}
-			
-			Iterator<Row>rows=sheet.iterator();
-			Row firstRow = rows.next();
-			Iterator<Cell>cells=firstRow.iterator();
-			
-			while(cells.hasNext()) {
-				Cell value = cells.next();
-				if(value.getStringCellValue().equalsIgnoreCase("testCaseId"));
+
+		// List to store values from the matched row
+		ArrayList<String> data = new ArrayList<String>();
+		logger.debug("Number of sheets in workbook: {}", sheets);
+
+		// Iterate through all sheets in the workbook
+		for (int i = 0; i < sheets; i++) {
+			if (workbook.getSheetName(i).equalsIgnoreCase(sheetName)) { // Check if the current sheet matches the
+																		// requested sheet name
+				logger.info("Reading data from sheet: {}", sheetName);
+				sheet = workbook.getSheetAt(i);
+
+				Iterator<Row> rows = sheet.iterator();
+				Row firstRow = rows.next(); // First row is expected to be the header row
+				Iterator<Cell> cells = firstRow.iterator();
+				int k = 0; // counter to track column index
+				int column = 0; // Will store the index of "testCaseId" column
+
+				// Iterate through header cells to find the "testCaseId" column
+				while (cells.hasNext()) {
+					Cell value = cells.next();
+
+					if (value.getStringCellValue().equalsIgnoreCase("testCaseId")) {
+						column = k;
+						logger.debug("'testCaseId' column found at index {}", column);
+					}
+					k++;
+				}
+				// Look through remaining rows for the given test case ID
+				while (rows.hasNext()) {
+					Row r = rows.next();
+					// Check if this row contains the matching testCaseId
+					if (r.getCell(column).getStringCellValue().equalsIgnoreCase(testCaseIdName)) {
+						logger.debug("Found matching test case ID: {}", testCaseIdName);
+						// Read all cells in the matched row
+						Iterator<Cell> ce = r.cellIterator();
+						while (ce.hasNext()) {
+							Cell c = ce.next();
+							if (c.getCellType() == CellType.STRING) {
+								data.add(c.getStringCellValue());
+							} else {
+								data.add(NumberToTextConverter.toText(c.getNumericCellValue()));
+							}
+						}
+					} else {
+						logger.warn("Test case ID '{}' not found in row {}", testCaseIdName, r.getRowNum());
+					}
+				}
 			}
 		}
-		
-		
-		return null;
-		
+
+		logger.info("Data for TestCaseID: " + testCaseIdName);
+		for (String value : data) {
+			System.out.println(value);
+		}
+		return data;
+
 	}
 
 }
